e0048f6782ad6b637db06c01747feb8fc9c81cff
loitr <loitruong2345@gmail.com>
2025-04-07 12:51:30 +0700

    Add core_cm4.h for CMSIS Cortex-M4 support
    Added core_cm4.h to provide the Cortex-M4 core peripheral access layer. This includes definitions for registers, bit manipulation macros, and core functionality for use with CMSIS. Essential for enabling low-level hardware access on Cortex-M4 based systems.

---
+++ Core/Src/main.c
@@ -0,0 +1,451 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdint.h>
+#include <stddef.h>
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+typedef struct {
+    uint8_t low;
+    uint8_t high;
+} crc_result_t;
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define Slave_Address           27 //
+#define Oper_Time_Out           5  // 5ms
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+crc_result_t get_CRC16(const uint8_t* data, size_t length);
+void Process_Read_Holding_Register(void);
+void Process_Preset_Single_Register(void);
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
+
+UART_HandleTypeDef huart4;
+
+/* USER CODE BEGIN PV */
+unsigned int holding_register[0xFF];
+unsigned char modbus_frame[8];
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_UART4_Init(void);
+static void MX_TIM2_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_UART4_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
+  // Listening to the network
+  HAL_UART_Receive_IT(&huart4, (uint8_t*)modbus_frame, 1);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    if (holding_register[0x00] == 0x0001) {
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
+    }
+    else {
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+    }
+      if (holding_register[0x01] == 0x0001) {
+          HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7, GPIO_PIN_SET);
+      }
+      else {
+          HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7, GPIO_PIN_RESET);
+      }
+      if (holding_register[0x02] == 0x0001) {
+          HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, GPIO_PIN_SET);
+      }
+      else {
+          HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, GPIO_PIN_RESET);
+      }
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 0;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 9625;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief UART4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_UART4_Init(void)
+{
+
+  /* USER CODE BEGIN UART4_Init 0 */
+
+  /* USER CODE END UART4_Init 0 */
+
+  /* USER CODE BEGIN UART4_Init 1 */
+
+  /* USER CODE END UART4_Init 1 */
+  huart4.Instance = UART4;
+  huart4.Init.BaudRate = 9600;
+  huart4.Init.WordLength = UART_WORDLENGTH_8B;
+  huart4.Init.StopBits = UART_STOPBITS_2;
+  huart4.Init.Parity = UART_PARITY_NONE;
+  huart4.Init.Mode = UART_MODE_TX_RX;
+  huart4.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart4.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN UART4_Init 2 */
+
+  /* USER CODE END UART4_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7|GPIO_PIN_10, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PA7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PE7 PE10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PD12 PD13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if(huart->Instance == UART4)
+    {
+        uint8_t temp_buffer = 0x00;
+        if (modbus_frame[0] != Slave_Address) {      // Master is not talking to me
+            // Keep listening
+            HAL_UART_Receive_IT(&huart4, (uint8_t*)modbus_frame, 1);
+            return;
+        }
+        // Master is talking to me
+        HAL_UART_Receive(&huart4, &temp_buffer, 1, Oper_Time_Out);     // read function code
+        modbus_frame[1] = temp_buffer;
+        // process the function code
+        switch (modbus_frame[1]) {
+            case 0x03:      // Read Holding Register
+                Process_Read_Holding_Register();
+                break;
+            case 0x06:      // Preset Single Register
+                Process_Preset_Single_Register();
+                break;
+            default:        // Not recognize as a valid function code
+                // HAL_UART_Transmit(&huart4, (uint8_t*)"Invalid Function Code", 20, Oper_Time_Out);
+                break;
+        }
+        HAL_UART_Receive_IT(&huart4, (uint8_t*)modbus_frame, 1);        // restart listening
+    }
+}
+
+void Process_Read_Holding_Register(void)
+{
+    uint8_t temp_buffer[2] = {0x00, 0x00};
+    HAL_UART_Receive(&huart4, (uint8_t*)temp_buffer, 2, Oper_Time_Out);     // read address
+    modbus_frame[2] = temp_buffer[0];       // Data Address Hi
+    modbus_frame[3] = temp_buffer[1];       // Data Address Lo
+    HAL_UART_Receive(&huart4, (uint8_t*)temp_buffer, 2, Oper_Time_Out);     // read quantity
+    modbus_frame[4] = temp_buffer[0];       // Number of Points Hi
+    modbus_frame[5] = temp_buffer[1];       // Number of Points Lo
+    HAL_UART_Receive(&huart4, (uint8_t*)temp_buffer, 2, Oper_Time_Out);     // read CRC
+    modbus_frame[7] = temp_buffer[0];       // CRC Lo
+    modbus_frame[6] = temp_buffer[1];       // CRC Hi
+    // init response
+    int number_to_send = 5 + (modbus_frame[4] << 8 | modbus_frame[5]) * 2;
+    uint8_t response[number_to_send];
+    response[0] = Slave_Address;
+    response[1] = 0x03;
+    response[2] = number_to_send - 5;
+    // Check data
+    crc_result_t crc_result = get_CRC16((uint8_t*)modbus_frame, 6);
+    if (crc_result.low == modbus_frame[6] && crc_result.high == modbus_frame[7])
+        for (int i = 0; i < (modbus_frame[4] << 8 | modbus_frame[5]); i++) {
+            int index = 1 + (modbus_frame[2] << 8 | modbus_frame[3]) + i;
+            response[3 + i] = holding_register[index] >> 8;     // Data Hi
+            response[4 + i] = holding_register[index] & 0xFF;   // Data Lo
+        }
+    crc_result = get_CRC16((uint8_t*)response, number_to_send - 2);
+    response[number_to_send - 2] = crc_result.low;
+    response[number_to_send - 1] = crc_result.high;
+    HAL_UART_Transmit(&huart4, (uint8_t*)response, number_to_send, Oper_Time_Out);
+}
+void Process_Preset_Single_Register(void)
+{
+    uint8_t response[8];
+    uint8_t temp_buffer[6];
+    HAL_UART_Receive(&huart4, (uint8_t*)temp_buffer, 6, Oper_Time_Out);    // Read all bytes
+    // create echo response for function
+    response[0] = modbus_frame[0];
+    response[1] = modbus_frame[1];
+    for (int i = 0; i < 6; i++) {
+        response[i + 2] = temp_buffer[i];
+        modbus_frame[i + 2] = temp_buffer[i];       // update modbus frame
+    }
+    crc_result_t crc_result = get_CRC16((uint8_t*)modbus_frame, 6);
+    if (crc_result.low != modbus_frame[6] || crc_result.high != modbus_frame[7]) {
+        return;
+    }
+    // Preset value
+    holding_register[modbus_frame[3] << 8 | modbus_frame[2]] = modbus_frame[5] << 8 | modbus_frame[4];
+    crc_result = get_CRC16((uint8_t*)response, 6);
+    response[6] = crc_result.low;
+    response[7] = crc_result.high;
+    HAL_UART_Transmit(&huart4, (uint8_t*)response, 8, Oper_Time_Out);
+}
+crc_result_t get_CRC16(const uint8_t* data, size_t length)
+{
+    uint16_t crc = 0xFFFF;
+
+    for (size_t pos = 0; pos < length; pos++)
+    {
+        crc ^= data[pos];
+
+        for (int i = 8; i != 0; i--)
+        {
+            if ((crc & 0x0001) != 0)
+            {
+                crc >>= 1;
+                crc ^= 0xA001;
+            }
+            else
+            {
+                crc >>= 1;
+            }
+        }
+    }
+
+    crc_result_t result;
+    result.low = (uint8_t)(crc & 0xFF);
+    result.high = (uint8_t)(crc >> 8);
+
+    return result;
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
